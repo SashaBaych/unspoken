258196ae22efe3434d64b76d51e97e3d359e1001
Sasha Baych <zima.zimushka@gmail.com>
2024-12-18 15:37:31 +0100

    Initial Commit

---
+++ unspoken/AppModel.swift
@@ -0,0 +1,21 @@
+//
+//  AppModel.swift
+//  unspoken
+//
+//  Created by Sasha Baych on 12/18/24.
+//
+
+import SwiftUI
+
+/// Maintains app-wide state
+@MainActor
+@Observable
+class AppModel {
+    let immersiveSpaceID = "ImmersiveSpace"
+    enum ImmersiveSpaceState {
+        case closed
+        case inTransition
+        case open
+    }
+    var immersiveSpaceState = ImmersiveSpaceState.closed
+}
